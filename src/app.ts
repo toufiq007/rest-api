8a0397c2213e0b0adbee8a4e6299b55f5b3c99d6
toufiq007 <t.limon2342@gmail.com>
2024-12-05 23:47:12 +0600

    install required dependencies like body-parser,cookie-parser,cors,dotenv,compression

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log(`server is running`);
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+// using our middlewares
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+
+// port
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`server is running ${port}`);
 });
